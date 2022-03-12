c7e6e6b421d3f20db55604477d6c1493d4852343
hugosenari <hugosenari@gmail.com>
2022-03-12 03:51:00 -0300

    chore: re index packedargs

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''a convention mainly created for `createThread` proc'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."packedargs-main".url = "path:./main";
+  inputs."packedargs-main".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."packedargs-main".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ main/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = ''a convention mainly created for `createThread` proc'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-packedArgs-main.flake = false;
+  inputs.src-packedArgs-main.owner = "hamidb80";
+  inputs.src-packedArgs-main.ref   = "refs/heads/main";
+  inputs.src-packedArgs-main.repo  = "packedArgs";
+  inputs.src-packedArgs-main.type  = "github";
+  
+  inputs."macroplus".dir   = "nimpkgs/m/macroplus";
+  inputs."macroplus".owner = "riinr";
+  inputs."macroplus".ref   = "flake-pinning";
+  inputs."macroplus".repo  = "flake-nimble";
+  inputs."macroplus".type  = "github";
+  inputs."macroplus".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."macroplus".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-packedArgs-main"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-packedArgs-main";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
